d73b44d5d68c22e05ab613a84a9c0514456c353b
youkorr <96953496+youkorr@users.noreply.github.com>
2026-07-06 19:40:15 +0200

    Create sc2356.h

---
+++ components/esp_cam_sensor/sensor/sc2356/include/sc2356.h
@@ -0,0 +1,28 @@
+/*
+ * SPDX-FileCopyrightText: 2024 Espressif Systems (Shanghai) CO LTD
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+#pragma once
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "esp_cam_sensor_types.h"
+#include "sc2356_types.h"
+
+#define SC2356_SCCB_ADDR   0x36
+
+/**
+ * @brief Power on camera sensor device and detect the device connected to the designated sccb bus.
+ *
+ * @param[in] config Configuration related to device power-on and detection.
+ * @return
+ *      - Camera device handle on success, otherwise, failed.
+ */
+esp_cam_sensor_device_t *sc2356_detect(esp_cam_sensor_config_t *config);
+
+#ifdef __cplusplus
+}
+#endif
